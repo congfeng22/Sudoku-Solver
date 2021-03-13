ef87c3423a984e5ed019b255c25db31c21985458
congfeng22 <congfeng22@gmail.com>
2021-03-13 00:37:44 -0500

    added arrow, palindrome, xv features

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Sudoku-Solver
 Solves Sudoku and variants
 
-Currently supports the following extra rules: knight, king, diagonal, disjoint, nonconsecutive
+Currently supports the following extra rules: knight, king, diagonal, disjoint, nonconsecutive, arrow, palindrome, XV
--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -7,7 +7,7 @@ public class SudokuSolver {
         // If assignment is complete, return result
         if (isDone(s)) return s;
         // Move pointer to next empty square
-        while (s.getGrid().get(idx/9).get(idx%9) != -1) idx++;
+        while (s.getGrid().get(idx/9).get(idx%9) != 0) idx++;
         // Row and column for easy access
         int row = idx/9; int col = idx%9;
         // Copy current candidates for this square
@@ -32,7 +32,7 @@ public class SudokuSolver {
             }
             // Backtrack if doesn't work
             s.getCandidates().put(idx, tempCandidates);
-            s.getGrid().get(row).set(col, -1);
+            s.getGrid().get(row).set(col, 0);
             newCandidates.clear();
         }
         // No solution
@@ -99,7 +99,7 @@ public class SudokuSolver {
                 if (location == -1) continue;
                 for (int i=0; i<thermo.size(); i++){
                     int neighbor = grid.get(thermo.get(i)/9).get(thermo.get(i)%9);
-                    if (neighbor != -1){
+                    if (neighbor != 0){
                         if (i<location && neighbor >= value - (location-i-1)) return false;
                         if (i>location && value + (i-location-1) >= neighbor) return false;
                     }
@@ -143,6 +143,48 @@ public class SudokuSolver {
                 }
             }
         }
+
+        // Check arrow constraint
+        if (rules.containsKey("arrow")){
+            List<List<Integer>> arrowRules = rules.get("arrow");
+            for (List<Integer> arrow : arrowRules){
+                if (arrow.contains(row*9+col)){
+                    int arrowHead = grid.get(arrow.get(0)/9).get(arrow.get(0)%9)==0?9:grid.get(arrow.get(0)/9).get(arrow.get(0)%9);
+                    for (int i=1; i<arrow.size(); i++){
+                        arrowHead -= grid.get(arrow.get(i)/9).get(arrow.get(i)%9)==0?1:grid.get(arrow.get(i)/9).get(arrow.get(i)%9);
+                        if (arrowHead<0){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Check palindrome constraint
+        if (rules.containsKey("palindrome")){
+            List<List<Integer>> palindromeRules = rules.get("palindrome");
+            for (List<Integer> palindrome : palindromeRules){
+                if (palindrome.contains(row*9+col)){
+                    for (int i=0; i<palindrome.size()/2; i++){
+                        int start = grid.get(palindrome.get(i)/9).get(palindrome.get(i)%9);
+                        int end = grid.get(palindrome.get(palindrome.size()-i-1)/9).get(palindrome.get(palindrome.size()-i-1)%9);
+                        if (start != 0 && end != 0 && start != end) return false;
+                    }
+                }
+            }
+        }
+
+        // Check XV constraint
+        if (rules.containsKey("xv")){
+            List<List<Integer>> xvRules = rules.get("xv");
+            for (List<Integer> xv : xvRules){
+                int one = grid.get(xv.get(1)/9).get(xv.get(1)%9);
+                int two = grid.get(xv.get(2)/9).get(xv.get(2)%9);
+                if ((xv.get(1) == row*9+col && two != 0) || (xv.get(2) == row*9+col && one != 0)) {
+                    if (one + two != xv.get(0)) return false;
+                }
+            }
+        }
         return true;
     }
 
@@ -165,7 +207,7 @@ public class SudokuSolver {
         if (s == null) return false;
         for (List<Integer> row : s.getGrid()){
             for (int entry : row){
-                if (entry == -1){
+                if (entry == 0){
                     return false;
                 }
             }
@@ -184,8 +226,7 @@ public class SudokuSolver {
             List<Integer> row = new ArrayList<>();
             for (int j=0; j<9; j++){
                 char c = grid.charAt(9*i+j);
-                if (c == '0') row.add(-1);
-                else row.add(c-'0');
+                row.add(c-'0');
             }
             newGrid.add(row);
         }
@@ -194,39 +235,101 @@ public class SudokuSolver {
 
     public static void main(String[] args){
         // normal
-        List<List<Integer>> grid5 = gridParser("005009400407010002008020095004000070000607000060000200980060300300090104001800600");
-        List<List<Integer>> grid1 = gridParser("000000000058020760036000480007109800300000006010805020000010000702000508005204300");
+        List<List<Integer>> grid1 = gridParser("005009400407010002008020095004000070000607000060000200980060300300090104001800600");
+        List<List<Integer>> grid2 = gridParser("000000000058020760036000480007109800300000006010805020000010000702000508005204300");
+
         // knight
-        List<List<Integer>> grid6 = gridParser("000000000002300000010040000080050000007600000000000870000009006000002005000000340");
+        List<List<Integer>> grid3 = gridParser("000000000002300000010040000080050000007600000000000870000009006000002005000000340");
+
         // king
-        List<List<Integer>> grid3 = gridParser("000628000009000300060000010700000008400000005600000003080000090005000700000971000");
+        List<List<Integer>> grid4 = gridParser("000628000009000300060000010700000008400000005600000003080000090005000700000971000");
+
         // thermo
         //////////////////////////WARNING TIMEOUT/////////////////////////////////
-        List<List<Integer>> grid4 = gridParser("000000000000000000000900000000000005000000000000000300000000000000000800000009000");
+        List<List<Integer>> grid5 = gridParser("000000000000000000000900000000000005000000000000000300000000000000000800000009000");
         List<List<Integer>> thermoRules = new ArrayList<>();
         thermoRules.add(Arrays.asList(0, 10, 20, 30, 38, 46));
         thermoRules.add(Arrays.asList(8, 16, 24, 32, 22, 12));
         thermoRules.add(Arrays.asList(72, 64, 56, 48, 58, 68));
         thermoRules.add(Arrays.asList(80, 70, 60, 50, 42, 34));
+
         // diagonal
-        List<List<Integer>> grid7 = gridParser("000902000000807000900050003007409300800205007060000050200301004000504000009000700");
+        List<List<Integer>> grid6 = gridParser("000902000000807000900050003007409300800205007060000050200301004000504000009000700");
+
         // disjoint
-        List<List<Integer>> grid8 = gridParser("090040010100305006006000400000901050207030901000604000009000300700403009080060047");
+        List<List<Integer>> grid7 = gridParser("090040010100305006006000400000901050207030901000604000009000300700403009080060047");
+
         // nonconsecutive and knight
-        List<List<Integer>> grid9 = gridParser("000000000000000000000407000006000500000000000004000300000205000000000000000000000");
+        List<List<Integer>> grid8 = gridParser("000000000000000000000407000006000500000000000004000300000205000000000000000000000");
+
         // nonconsecutive and knight and king
-        List<List<Integer>> grid = gridParser("000000000000000000000000000000000000001000000000000200000000000000000000000000000");
+        List<List<Integer>> grid9 = gridParser("000000000000000000000000000000000000001000000000000200000000000000000000000000000");
+
+        // arrow
+        List<List<Integer>> grid10 = gridParser("000000000034700060000000070000000090000000000080000000090000000060009850000000000");
+        List<List<Integer>> arrowRules = new ArrayList<>();
+        arrowRules.add(Arrays.asList(10, 19, 28));
+        arrowRules.add(Arrays.asList(11, 20, 29));
+        arrowRules.add(Arrays.asList(12, 21, 30));
+        arrowRules.add(Arrays.asList(16, 15, 14));
+        arrowRules.add(Arrays.asList(25, 24, 23));
+        arrowRules.add(Arrays.asList(34, 33, 32));
+        arrowRules.add(Arrays.asList(46, 47, 48));
+        arrowRules.add(Arrays.asList(55, 56, 57));
+        arrowRules.add(Arrays.asList(64, 65, 66));
+        arrowRules.add(Arrays.asList(68, 59, 50));
+        arrowRules.add(Arrays.asList(69, 60, 51));
+        arrowRules.add(Arrays.asList(70, 61, 52));
+
+        // palindrome
+        List<List<Integer>> grid11 = gridParser("002030400000905000000000000000000000320000078000000000000000000000504000506000809");
+        List<List<Integer>> palindromeRules = new ArrayList<>();
+        palindromeRules.add(Arrays.asList(9, 19, 29));
+        palindromeRules.add(Arrays.asList(11, 21, 31));
+        palindromeRules.add(Arrays.asList(13, 23, 33));
+        palindromeRules.add(Arrays.asList(15, 25, 35));
+        palindromeRules.add(Arrays.asList(45, 55, 65));
+        palindromeRules.add(Arrays.asList(47, 57, 67));
+        palindromeRules.add(Arrays.asList(49, 59, 69));
+        palindromeRules.add(Arrays.asList(51, 61, 71));
+
+        // XV
+        List<List<Integer>> grid = gridParser("000000000000000000002000405000000000000000000000000000504000100000000000000000000");
+        List<List<Integer>> xvRules = new ArrayList<>();
+        xvRules.add(Arrays.asList(5,0,9));
+        xvRules.add(Arrays.asList(5,6,15));
+        xvRules.add(Arrays.asList(5,14,15));
+        xvRules.add(Arrays.asList(5,21,30));
+        xvRules.add(Arrays.asList(5,28,37));
+        xvRules.add(Arrays.asList(5,44,53));
+        xvRules.add(Arrays.asList(5,49,58));
+        xvRules.add(Arrays.asList(5,6,15));
+        xvRules.add(Arrays.asList(10,2,3));
+        xvRules.add(Arrays.asList(10,7,8));
+        xvRules.add(Arrays.asList(10,4,13));
+        xvRules.add(Arrays.asList(10,15,16));
+        xvRules.add(Arrays.asList(10,23,32));
+        xvRules.add(Arrays.asList(10,31,32));
+        xvRules.add(Arrays.asList(10,34,35));
+        xvRules.add(Arrays.asList(10,36,37));
+        xvRules.add(Arrays.asList(10,42,43));
+        xvRules.add(Arrays.asList(10,40,49));
+        xvRules.add(Arrays.asList(10,41,50));
+        xvRules.add(Arrays.asList(10,57,58));
+        xvRules.add(Arrays.asList(10,61,62));
+        xvRules.add(Arrays.asList(10,63,64));
+        xvRules.add(Arrays.asList(10,66,67));
+        xvRules.add(Arrays.asList(10,70,71));
+        xvRules.add(Arrays.asList(10,79,80));
 
         Map<String, List<List<Integer>>> rules = new HashMap<>();
         rules.put("normal", null);
-        rules.put("nonconsecutive", null);
-        rules.put("knight", null);
-        rules.put("king", null);
-        //rules.put("thermo", thermoRules);
+        //rules.put("nonconsecutive", null);
+        rules.put("xv", xvRules);
 
         Map<Integer, Set<Integer>> candidates = new HashMap<>();
         for (int i=0; i<81; i++){
-            if (grid.get(i/9).get(i%9) == -1) {
+            if (grid.get(i/9).get(i%9) == 0) {
                 candidates.put(i, Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9).collect(Collectors.toCollection(HashSet::new)));
             }
             else {
